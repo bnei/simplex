88fdc91bb673f37b2e3d228701d5e31d7633d030
bnei <briannei@gmail.com>
2021-01-10 16:55:49 -0800

    1-10 Commmit

--- simplex.py
+++ simplex.py
@@ -3,8 +3,9 @@
 print("How many decision variables do you have?")
 numVar = input()
 
-while (targetType != "min" or "max")
-print("Is this is a min(imization) or max(imization) problem?")
+while (targetType != "min" or "max"):
+    print("Is this is a min(imization) or max(imization) problem?")
+
 
 decVar = []
-print("There are "numVar "variables")
+print("There are " + numVar + "variables")
