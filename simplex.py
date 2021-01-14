638a30f5a02d52ae26e03f7c8f619e9b64c98a65
bnei <briannei@gmail.com>
2021-01-13 23:06:15 -0800

    1-13

--- simplex.py
+++ simplex.py
@@ -1,12 +1,23 @@
-#Get User to Define Variables
+#Get user to define variables
 
 print("How many decision variables do you have?")
-numVar = input()
+numVar = int(input())
 
 targetType = "null"
-while (targetType != ("min" or "max")):
+while (targetType != "min" and targetType != "max"):
     print("Is this is a min(imization) or max(imization) problem?")
     targetType = input()
 
-decVar = []
-print("There are " + numVar + " variables")
+decVar = ["A"] * numVar
+
+#Set up Target Function
+print("Target Function Setup:")
+
+for i in range(numVar):
+    while (isinstance(decVar[i], int) == False):
+        print("Enter coefficient for variable", (i+1))
+        decVar[i] = int(input())
+
+print (decVar)
+
+
